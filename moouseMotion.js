850478883ce9e0d7a937a01a2e39888ead658593
fefwee <aweo.wew@mail.ru>
2022-10-16 21:20:52 +0300

    свойства и методы мыши

---
+++ moouseMotion.js
@@ -0,0 +1,18 @@
+class mouseMove{
+    constructor(rightBtn,leftBtn,rollBtn){
+        this.rightBtn=rightBtn;
+        this.leftBtn=leftBtn;
+        this.rollBtn=rollBtn;
+    }
+    diRight(){
+
+    }
+    doLeft(){
+
+    }
+    doRoll(){
+        
+    }
+
+
+}
